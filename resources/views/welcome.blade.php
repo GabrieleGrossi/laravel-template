09af31b59eecde04e16b0849be4deb0d252e92f1
GabrieleGrossi <grossigabriele98@gmail.com>
2023-07-19 15:06:57 +0200

    fixed error in typing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,16 @@
 @extends('layouts.app')
 @section('title', 'Welcome')
-@section('main-section')
+@section('main-content')
     <h1>
         Welcome
     </h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3>
+                    Ciao
+                </h3>
+            </div>
+        </div>
+    </div>
 @endsection
