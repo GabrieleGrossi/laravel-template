926c81739774b0d75e0e4946abdc97582fb62923
GabrieleGrossi <grossigabriele98@gmail.com>
2023-07-19 14:58:19 +0200

    genral creation of a template per prox exercise

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+import * as bootstrap from 'bootstrap';
+//per gli alias, ricordarsi di importarlo qui così: " import '~resources/scss/app.scss'; "
+
+import.meta.glob([
+    '../img/**'
+]);
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+const path = require('path');
+
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/css/app.css',
+        laravel({input: [
+            'resources/scss/app.scss',
             'resources/js/app.js',
-        ]),
+        ],
+        refresh:true
+    }),
     ],
+    resolve:{
+        alias:{
+            //ricordarsi di portarlo anche in app.js (lo trovi nella cartella resources/js/app.js) 
+            //esempio: '~resources' : '/resources/',
+
+            '~bootstrap' : path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    }
 });
