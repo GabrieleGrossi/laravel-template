853717f64a730d5200477619e9ea37d271bc790c
GabrieleGrossi <grossigabriele98@gmail.com>
2023-07-22 13:45:23 +0200

    Add some generic details (img, header, footer)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,11 @@
     <title>@yield('title', 'Laravel')</title>
 </head>
 <body>
-    
+    @include ('partials.header')
     <main>
         @yield('main-content')
     </main>
-
+    @include('partials.footer')
     @vite('resources/js/app.js')
 </body>
 </html>
